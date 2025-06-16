bb66e59215d66a4b139bf1131fe7765fa85ae622
khakimovallamurod <allamurod0206@gmail.com>
2025-06-16 15:00:25 +0500

    get token

--- config.py
+++ config.py
@@ -8,9 +8,4 @@ def get_token():
         raise ValueError("TOKEN not found.")
     return TOKEN
 
-def get_url():
-    URL = os.getenv("url")
-    if URL is None:
-        raise ValueError("URL not found.")
-    return URL
 
