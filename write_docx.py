433199796ac668584f12c5ce051ce43f4eeba185
khakimovallamurod <allamurod0206@gmail.com>
2025-06-23 23:37:25 +0300

    change format for new docx

--- write_docx.py
+++ write_docx.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from docx import Document
-from docx.shared import Pt, Inches
+from docx.shared import Pt, Inches, Cm
 from docx.enum.section import WD_ORIENT
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.oxml.ns import qn
@@ -32,7 +32,7 @@ def date_to_string(date_obj):
         "Iyul", "Avgust", "Sentabr", "Oktabr", "Noyabr", "Dekabr"
     ]
     month_name = month_names[month - 1]
-    return f"{year} yil {day}-{month_name.lower()} dagi"
+    return f"{year} yil {day}-{month_name.lower()}dagi"
 
 
 def create_diplom_kuchirma_hujjat(student_data, sana,  fayl_nomi='bitiruvchi_diplom_kuchirma.docx'):
@@ -40,12 +40,12 @@ def create_diplom_kuchirma_hujjat(student_data, sana,  fayl_nomi='bitiruvchi_dip
 
     section = doc.sections[0]
     section.orientation = WD_ORIENT.LANDSCAPE
-    section.page_width = Inches(11.69)
-    section.page_height = Inches(8.27)
-    section.top_margin = Inches(0.39)
-    section.bottom_margin = Inches(0.39)
-    section.left_margin = Inches(0.39)
-    section.right_margin = Inches(0.39)
+    section.page_width = Cm(29.7)   # A4 gorizontal eni
+    section.page_height = Cm(21.0)  # A4 gorizontal bo‘yi
+    section.top_margin = Cm(1)
+    section.bottom_margin = Cm(1)
+    section.left_margin = Cm(1)
+    section.right_margin = Cm(1)
 
     def add_line(cell, text, size=12, bold=False, center=True):
         p = cell.add_paragraph()
@@ -60,35 +60,88 @@ def create_diplom_kuchirma_hujjat(student_data, sana,  fayl_nomi='bitiruvchi_dip
 
         p_format = p.paragraph_format
         p_format.line_spacing = Pt(12)
+        p_format.space_before = Pt(4)  
+        p_format.space_after = Pt(4)
 
+    def add_line_mixed(cell, parts, center=True):
+        p = cell.add_paragraph()
+        p.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER if center else WD_PARAGRAPH_ALIGNMENT.LEFT
+
+        p_format = p.paragraph_format
+        p_format.line_spacing = Pt(12)
+        p_format.space_before = Pt(4)
+        p_format.space_after = Pt(4)
+
+        for text, bold, size in parts:
+            run = p.add_run(text)
+            run.font.name = 'Times New Roman'
+            run.font.size = Pt(size)
+            run.bold = bold
+            run._element.rPr.rFonts.set(qn('w:eastAsia'), 'Times New Roman')
+            run._element.rPr.rFonts.set(qn('w:ascii'), 'Times New Roman')
+            run._element.rPr.rFonts.set(qn('w:hAnsi'), 'Times New Roman')
+
+    def add_empty_line(cell, size=6):
+        p = cell.add_paragraph()
+        run = p.add_run(" ")  # faqat bitta bo‘sh joy, matnsiz
+        run.font.name = 'Times New Roman'
+        run.font.size = Pt(size)
+        
+        p.paragraph_format.line_spacing = Pt(10)
+        p.space_before = Pt(0)  
+        p.space_after = Pt(0)
+        p.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
 
     def fill_cell(cell, item):
-        add_line(cell, "O‘ZBEKISTON RESPUBLIKASI", 12, bold=True)
-        add_line(cell, "BAKALAVR", 12, bold=True)
-        add_line(cell, "DIPLOMI", 12, bold=True)
+        for para in cell.paragraphs:
+            p = para._element
+            p.getparent().remove(p)
+        add_line(cell, "O‘ZBEKISTON RESPUBLIKASI", 16, bold=True)
+        add_line(cell, "BAKALAVR", 16, bold=True)
+        add_line(cell, "DIPLOMI", 16, bold=True)
         qaror_id = item['diplom_id'] if item['diplom_id'] != '' else "B №____________"
-        add_line(cell, qaror_id, 14)
+        add_line(cell, f'  {qaror_id}', 16)
+        add_empty_line(cell, 9)
         add_line(cell, "K O‘ Ch I R M A", 16, bold=True)
         add_line(cell, "SAMARQAND DAVLAT UNIVERSITETI", 14, bold=True)
+        add_empty_line(cell, 14)
         add_line(cell, "Davlat attestasiya komissiyasining", 14, bold=True)
         add_line(cell, f"{sana}", 14, bold=True)
-        add_line(cell, "qaroriga  binoan", 14, bold=True)
+        add_line(cell, "qaroriga binoan", 14, bold=True)
+        add_empty_line(cell, 10)
         fish = item['F.I.Sh.'] if item['F.I.Sh.'] != '' else "____________________________________"
-        add_line(cell, f"{fish}ga", 14)
+        add_line_mixed(cell, [
+            (fish.upper(), True, 16),
+            ("ga", False, 14)
+        ])
+        add_empty_line(cell, 11)
         yunalish = item['Yonalish'] if item['Yonalish'] != '' else "____________________________________"
-        add_line(cell, f"{yunalish} yo’nalishi bo’yicha", 14)
-        add_line(cell, "B A K A L A V R", 16, bold=True)
-        add_line(cell, "DARAJASI", 14, bold=True)
+        add_line(cell, f"{yunalish}", 16)
+        add_line(cell, f"  yo’nalishi bo’yicha", 14)
+        add_empty_line(cell, 11)
+        add_line(cell, "B A K A L A V R", 20, bold=True)
+        add_line(cell, "DARAJASI", 16, bold=True)
+        add_empty_line(cell, 8)
         kval = item['Kvalifikatsiya'] if item['Kvalifikatsiya'] != '' else "____________________________________"
-        add_line(cell, f"va {kval} kvalifikatsiyasi berildi", 14)
+        add_line_mixed(cell, [
+            ('va ', False, 18),
+            (kval, True, 16)
+        ])
+        add_line(cell, f"kvalifikatsiyasi berildi", 18)
+        add_empty_line(cell, 10)
         tartib_raqam = item['tartib_raqam'] if item['tartib_raqam'] != '' else "________________"
-        add_line(cell, f"Ro‘yxatga olish raqami {tartib_raqam}", 14, center=False)
+        add_line_mixed(cell, [
+            ('      Ro‘yxatga olish raqami: ', False, 14),
+            (f'{tartib_raqam}', True, 14)
+        ], center=False)
+        add_empty_line(cell, 9)
         id = item['ID'] if item['ID'] != '' else "________"
-        add_line(cell, f"    Ushbu ko‘chirma faqat {id} sonli yo‘llanma bilan o‘z kuchiga ega.", 14, center=False)
-        add_line(cell, "Davlat attestatsiya va taqsimot", 14, center=False)
-        add_line(cell, "komissiyalari raisi.", 14, center=False)
-        add_line(cell, "Rektor                          R.I.Xalmuradov", 14)
-
+        add_line(cell, f"      Ushbu ko‘chirma faqat {id}-sonli yo‘llanma bilan o‘z kuchiga ega.", 14, center=False)
+        add_empty_line(cell, 9)
+        add_line(cell, "      Davlat attestatsiya va taqsimot", 14, center=False)
+        add_line(cell, "      komissiyalari raisi.", 14, center=False)
+        add_line(cell, "           Rektor                          R.I.Xalmuradov", 14, bold=True)
+        
     # Talabalarni juftlab sahifalarga joylashtirish
     for i in range(0, len(student_data), 2):
         table = doc.add_table(rows=1, cols=2)
@@ -114,6 +167,6 @@ def main(file_path, sana):
     return output_file
 
 if __name__ == "__main__":
-    file_path = 'битирувчи 2024-2025 кит чун АСЛ (2).xlsx'  # Replace with your actual file path
+    file_path = '2025 yil 10 iyun.xlsx'  # Replace with your actual file path
     sana = date(2023, 10, 1)  
     main(file_path, sana)
